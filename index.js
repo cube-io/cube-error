bb485e820b620664862ccbec1e23c256d5f5d0a7
Sven J. Hermann <sjh@justfone.dk>
2015-10-20 15:56:23 +0200

    add more safe .is-checker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     NotFound: require("./lib/NotFoundError.js"),
     Http: require("./lib/HttpError.js"),
-    Custom: require("./lib/CustomError.js")
+    Custom: require("./lib/CustomError.js"),
+    is: require("./lib/is.js")
 };
